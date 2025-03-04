4c0c61b512e83d57926ad3d2693b89bce9b60d18
Khazeral <wandehousse@gmail.com>
2025-03-04 11:23:45 +0100

    feat : add localStorage

--- src/projects.js
+++ src/projects.js
@@ -1,24 +1,24 @@
 import { Task } from "./task";
 
-
 export class Project {
-    constructor(title, color, tasks) {
+    constructor(title, color, tasks = []) {
         this.id = Date.now();
-        this.color = color
         this.title = title;
-        this.tasks = tasks;
+        this.color = color;
+        this.tasks = tasks.map(t => new Task(t.title, t.completed));
     }
 
-    addTask(task) {
+    addTask(task, projectManager) {
         if (!(task instanceof Task)) {
             throw new Error("Only Task instances can be added.");
         }
         this.tasks.push(task);
+        projectManager?.saveProjectsToLocalStorage();
     }
-    
 
-    removeTask(task) {
+    removeTask(task, projectManager) {
         this.tasks = this.tasks.filter(t => t !== task);
+        projectManager?.saveProjectsToLocalStorage(); 
     }
 
     getTasks() {
@@ -28,22 +28,48 @@ export class Project {
 
 export class ProjectManager {
     constructor() {
-        this.projects = [];
+        this.projects = this.loadProjectsFromLocalStorage();
     }
 
+    loadProjectsFromLocalStorage() {
+        const data = JSON.parse(localStorage.getItem("projects")) || [];
+    
+        this.projects = data.map(projectData => {
+            const project = new Project(projectData.title, projectData.color, 
+                projectData.tasks.map(taskData => new Task(taskData.title, taskData.completed))
+            );
+    
+            return project;
+        });
+
+        return this.projects
+
+    }
+    
+
+    saveProjectsToLocalStorage() {
+        const projects = this.getAllProjects().map(project => {
+            return {
+                ...project,
+                tasks: project.tasks.map(task => ({
+                    ...task,
+                    project: undefined,
+                }))
+            };
+        });
+    
+        localStorage.setItem("projects", JSON.stringify(projects));
+    }
+    
+
     addProject(project) {
-        if (!(project instanceof Project)) {
-            throw new Error("Only Project instances can be added.");
-        }
         this.projects.push(project);
+        this.saveProjectsToLocalStorage();
     }
 
     removeProject(project) {
         this.projects = this.projects.filter(p => p.id !== project.id);
-    }
-
-    getProjectByName(title) {
-        return this.projects.find(p => p.title === title);
+        this.saveProjectsToLocalStorage();
     }
 
     getAllProjects() {
